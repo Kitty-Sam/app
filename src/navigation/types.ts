c4707ef985ceb386c78ace382811f7058d08d99b
kitty-sam <samutic40@gmail.com>
2022-03-24 13:47:16 +0300

    move enum type

--- src/navigation/types.ts
+++ src/navigation/types.ts
@@ -1,4 +1,15 @@
+import { BottomTabNavigationOptions } from '@react-navigation/bottom-tabs';
+import { TAB_NAVIGATION_NAME } from '../enum/enum';
+import { RouteProp } from '@react-navigation/native';
+
 export type TabNavigationParamList = {
   CityScreen: undefined;
   List: undefined;
 };
+
+export type ScreenOptionsType =
+  | BottomTabNavigationOptions
+  | ((props: {
+      navigation: TAB_NAVIGATION_NAME;
+      route: RouteProp<TabNavigationParamList, keyof TabNavigationParamList>;
+    }) => BottomTabNavigationOptions);
