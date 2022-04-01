379287e91c41b53adc300d69bc4c1096d3f86081
kitty-sam <samutic40@gmail.com>
2022-04-01 16:22:18 +0300

    plug redux-persist

--- index.js
+++ index.js
@@ -6,12 +6,16 @@ import { AppRegistry } from 'react-native';
 import { App } from './App';
 import { name as appName } from './app.json';
 import { Provider } from 'react-redux';
-import { store } from './src/store/store';
+
+import { PersistGate } from 'redux-persist/integration/react';
+import { persistor, store } from './src/store/configureStore';
 
 const ReduxProvider = () => {
   return (
     <Provider store={store}>
-      <App />
+      <PersistGate loading={null} persistor={persistor}>
+        <App />
+      </PersistGate>
     </Provider>
   );
 };
--- src/store/configureStore.ts
+++ src/store/configureStore.ts
@@ -1,16 +1,17 @@
 import AsyncStorage from '@react-native-async-storage/async-storage';
 import { createStore } from 'redux';
-import {
-  persistReducer,
-  persistStore,
-  persistCombineReducers,
-} from 'redux-persist';
-import { cityReducer } from './reducers/cityReducer';
+import { persistReducer, persistStore } from 'redux-persist';
+import { rootReducer } from './store';
 
 const persistConfig = {
   key: 'root',
   storage: AsyncStorage,
 };
+
+const pReducer = persistReducer(persistConfig, rootReducer);
+
+export const store = createStore(pReducer);
+export const persistor = persistStore(store);
 
 /*
 const rootReducer = persistCombineReducers(persistConfig, {
